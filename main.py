da4ec0b5d077ff5c49ddd3fe231eec63b53746b8
Fernando Ferraz Ribeiro <ffribeiro@gmai.com>
2021-06-18 10:08:02 -0300

    contador

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.metrics import dp
-from kivy.properties import StringProperty, BooleanProperty
+from kivy.properties import StringProperty, BooleanProperty, IntegerProperty
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -10,8 +10,12 @@ from kivy.uix.widget import Widget
 
 
 class WidgetsExample(GridLayout):
-    my_text = StringProperty
+    my_text = StringProperty('<-- clique no botão')
+    count = 0
+    font_size = IntegerProperty(100)
     def on_button_click(self):
+        self.count += 1
+        self.my_text = (str(self.count))
         print('clicked!')
     
 
