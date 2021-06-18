79549f736b7e8055aeab2c76f4ca78d32d2ae3c1
Fernando Ferraz Ribeiro <ffribeiro@gmai.com>
2021-06-18 09:48:33 -0300

    update

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from kivy.app import App
+from kivy.metrics import dp
+from kivy.properties import StringProperty, BooleanProperty
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
 
-class MainWidget(Widget):
-    pass
-
+class WidgetsExample(GridLayout):
+    my_text = StringProperty
+    def on_button_click(self):
+        print('clicked!')
+    
 
 class TheLabApp(App):
     pass
